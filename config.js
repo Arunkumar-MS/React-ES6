0aadc7b55f6110843b7fcf58a56a00db2d1c1a4e
Arunkumar <arunkumar.shivanna@in.tesco.com>
2015-07-21 08:25:57 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports =
 {
     productApiUrl: 'https://s.tesco.pl/api/v1/search/products?query=',
-    Appkey: 'trn:tesco:cid:mweb:uuid:A5EA1E42-1A9D-4262-8370-770B927D12E0'
+    Appkey: 'key'
 
 
 }
