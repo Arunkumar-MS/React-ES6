f285aab0b5277dd2327ec7a3a0a032e222ecc198
Arunkumar <arunkumar.shivanna@in.tesco.com>
2015-07-22 11:19:03 +0100

    updated

--- Assets/Components/plp.jsx
+++ Assets/Components/plp.jsx
@@ -29,6 +29,26 @@ class Result1 extends React.Component {
                                 <h4><a onClick={this.productDiscription.bind(item.product.id)} href="#">{item.product.title}</a></h4>
 
                                 <p> {item.product.description }  </p>
+
+                                <div className="input-group">
+          <span className="input-group-btn">
+              <button type="button" className="btn btn-danger btn-number"  data-type="minus" data-field="quant[2]">
+                  <span className="glyphicon glyphicon-minus"></span>
+              </button>
+          </span>
+                                    <input type="text" name="quant[2]" className="form-control input-number" value="1" min="1" max="100">
+                                    </input>
+
+
+          <span className="input-group-btn">
+              <button type="button" className="btn btn-success btn-number" data-type="plus" data-field="quant[2]">
+
+                  <span className="glyphicon glyphicon-plus"></span>
+              </button>
+          </span>
+                                    </div>
+
+
                                 <input type="button" className="btn btn-primary add" value="add">
                                 </input>
                             </div>
