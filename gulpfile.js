cfd9ef2284ed25605cac5d95d6b05013967f6f20
murariamar <amar.murari@in.tesco.com>
2015-07-31 06:52:09 +0100

    Fix Auto bundle
    Fixed gulp to watch jsx file changes and auto build

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,45 @@
+var fs = require('fs');
 var gulp = require('gulp');
+var gulpUtil = require('gulp-util');
 var browserify = require('browserify');
 var babelify = require('babelify');
 var source = require('vinyl-source-stream');
+var watchify = require('watchify');
+var path = require('path');
+var debug = require('gulp-debug');
 
-// Define some paths.
-var paths = {
-app_js: ['./Assets/Components/*.jsx'],
-//js: ['./Assets/build'],
-};
-
-
-gulp.task('build', function () {
+debugger;
+function getBundler() {
+  return (
     browserify({
         entries: 'Assets/Components/index.jsx',
         extensions: ['.jsx'],
-        debug: true
-    })
-        .transform(babelify)
-        .bundle()
-        .pipe(source('bundle.js'))
-        .pipe(gulp.dest('Assets/build'));
-});
+        debug : true })
+    .transform(babelify));
+}
 
-// Rerun tasks whenever a file changes.
-gulp.task('watch', function() {
-  //gulp.watch(paths.css, ['css']);
-  gulp.watch(paths.js, ['js']);
-});
+function update(bundler) {
+  bundler
+  .bundle()
+  .on('error', gulpUtil.log)
+  .on('end', function() {
+    return gulpUtil.log('Bundle complete');
+  })
+  .pipe(source('bundle.js'))
+  .pipe(debug({title: 'unicorn:'}))
+  .pipe(gulp.dest('Assets/build'));
+}
 
-gulp.task('default', ['watch', 'build']);	
+  gulp.task('browserify', function() {
+    return update(getBundler());
+  });
+
+  gulp.task('watch', function(){
+    var watch = watchify(getBundler());
+    watch.on('update', function(){
+      return update(watch);
+    });
+    return update(watch);
+  });
+
+gulp.task('default', ['browserify', 'watch'])
