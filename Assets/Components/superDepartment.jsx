4f057f211ef9bfb7c2e7cf85dd9f54350c487a41
Rakesh Nayak <Rakesh.Nayak@in.tesco.com>
2015-08-15 21:28:39 +0530

    Superdepartment Order fixed

--- Assets/Components/superDepartment.jsx
+++ Assets/Components/superDepartment.jsx
@@ -3,11 +3,10 @@ import React from 'react';
 class SuperDepartment extends React.Component {
     render() {
         var superDepartment = [];
-        for (var i = this.props.menuItems.length - 1; i >= 0; i--) {
-            var item = this.props.menuItems[i];
-            if(item.catId == null && item.parent==null)
-            {
-                superDepartment.push(item);
+        for (var i=0; i < this.props.menuItems.length ; i++) {
+                var item = this.props.menuItems[i];
+                if(item.catId == null && item.parent==null){
+                    superDepartment.push(item);
             }
         };
         document.getElementById('departmentMenu').style.display='block';
