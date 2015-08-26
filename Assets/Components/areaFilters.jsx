d7ec9004312e18aefe469f1e93472bfd0ca3e8b8
murariamar <amar.murari@in.tesco.com>
2015-08-26 14:03:37 +0100

    Recommit area filters

--- Assets/Components/areaFilters.jsx
+++ Assets/Components/areaFilters.jsx
@@ -1,21 +1,76 @@
 import React from 'react';
 import request from 'request';
+//import {searchData , getProducts} from './Action/searchAction';
+//import events from './Store/searchStore';
 
 class AreaFilters  extends React.Component
 {
+ /*constructor(props) {
+        super();
+        this.state = { aisleId: null, facets: props.facets, category: props.category, serachTerm: props.searchTerm }
+        this._onFacetsUpdated = this._onFacetsUpdated.bind(this);
+    }
+  propTypes:{
+        department  : React.PropTypes.String,
+        aisle       : React.PropTypes.String,
+        shelf       : React.PropTypes.String,
+    }
+
+	handleClick (categoryId, categoryName){
+		var searchQuery= this.props.searchTerm;
+
+		if(categoryId === null && categoryName === null)
+		{
+			searchData(searchQuery);
+		}
+		else if(this.props.category === 'Department')
+		{
+			this.setState({
+				departmentId:categoryId,
+				departmentName:categoryName
+			});
+			searchData(searchQuery, 1, categoryId);
+		}
+		else if(this.props.category === 'Aisle')
+		{
+			this.setState({
+				aisleId:categoryId,
+				aisleName:categoryName
+			});
+			searchData(searchQuery, 1, this.state.departmentId, categoryId);
+		}
+	}
+
+	_onFacetsUpdated(){
+		let data;
+		data = JSON.parse(event.target.response);
+		console.log(data);
+		this.setState({ aisleId: null, facets: data.facet.facets, category: data.facet.category, serachTerm: data.searchTerm });
+	}*/
+
  render() {
         return (
 				<div>
 					<h3>Area</h3>
 					<ul>
-						{this.props.facets.map(function (facet){
-							return(<li>{facet.facetName}</li>);
-						})}
+						{(this.props.category === 'Aisle') ? <li><a onClick={this.props.onBackToSearchResults.bind(null)}>Back to search results</a></li> : null}
+						{(this.props.category === 'Shelf') ? <li><a onClick={this.props.onBackToDepartment.bind(null)}>Back to {this.props.department}</a></li> : null}
+						{this.props.facets.map( function (facet){
+							return(<li><a onClick={this.props.onAreaFilterSelected.bind(null, facet.facetId, facet.facetName, this.props.category)}>
+								{facet.facetName} ({facet.binCount})</a></li>);
+						}, this)}
 						</ul>
 		        </div>		
         );
     }
+
+	//{(this.state.aisleId !== null) ? <li><a onClick={this.handleClick.bind(this, this.state.aisleId, this.state.aisleName)}>Back to {this.state.aisleName}</a></li> : null}
+	//{(this.props.category === 'Shelf') ? <li><a onClick={this.handleClick.bind(this, this.props.departmentId, this.state.departmentName)}>Back to {this.state.aisleName}</a></li> : null}
+    /*componentWillMount(){
+	events.addChangeListener(this._onFacetsUpdated, 'facetsUpdated');
+	}*/
 }
+
 
 
 export default AreaFilters;
--- Assets/Components/filters.jsx
+++ Assets/Components/filters.jsx
@@ -1,21 +1,57 @@
-import React from 'react';
-import request from 'request';
-import AreaFilters from './areaFilters';
+var React = require('react');
+var request = require('request');
+var AreaFilters = require('./areaFilters');
+var {searchData , getProducts} = require('./Action/searchAction');
+//import events from './Store/searchStore';
 
-class Filters  extends React.Component
+var Filters = React.createClass(  //extends React.Component
 {
+    getInitialState(){
+        return {
+                //initailFacetList: this.props.facetLists,
+                departmentId: null,
+                departmentName: null,
+                aisleId: null,
+                aisleName: null
+            };
+    },
+
  render() {
+    var areaFacet = this.props.facetLists.filter(function(facet){
+                return (facet.category !== 'Brand' && facet.category !== 'Promotion');
+            });
         return (
-        	<div>
-    			{this.props.facetLists.map(function (facet, i) {
-    				return(<div>
-    					{facet.category === 'Department' ? <AreaFilters facets={facet.facets} /> : null}
-    					</div>);
-    			})}
+            <div>
+                 <AreaFilters facets={areaFacet[0].facets} category={areaFacet[0].category} searchTerm={this.props.searchTerm} 
+                 onAreaFilterSelected={this._onAreaFilterSelected} onBackToSearchResults={this._onBackToSearchResults}
+                 onBackToDepartment={this._onBackToDepartment} department={this.state.departmentName}/>
             </div>
         );
+    },
+
+    _onAreaFilterSelected(selectedFilterId, selectedFilterName, category){
+        if(category === 'Department')
+        {
+            this.setState({departmentId: selectedFilterId, departmentName: selectedFilterName, aisleId: null, aisleName: null});
+            searchData(this.props.searchTerm, 1, selectedFilterId);
+        }
+        else if(category === 'Aisle')
+        {
+            this.setState({aisleId: selectedFilterId, aisleName: selectedFilterName});
+            searchData(this.props.searchTerm, 1, this.state.departmentId, selectedFilterId);
+        }
+    },
+
+    _onBackToDepartment(selectedFilterId, selectedFilterName){
+         this.setState({departmentId: selectedFilterId, departmentName: selectedFilterName, aisleId: null, aisleName: null});
+         searchData(this.props.searchTerm, 1, selectedFilterId);
+    },
+
+    _onBackToSearchResults(){
+        this.setState({selectedFilterId: null, selectedFilterName: null});
+        searchData(this.props.searchTerm, 1, null, null);
     }
-}
+});
 
 
-export default Filters;
+module.exports = Filters;
