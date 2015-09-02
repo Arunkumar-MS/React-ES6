40e7e2056b5d86ccd77130255e8c2d6339bdd5c3
Rakesh Nayak <rakesh.nayak@in.tesco.com>
2015-09-02 12:19:38 +0100

    BasketItems view included

--- Assets/Components/basketItems.jsx
+++ Assets/Components/basketItems.jsx
@@ -3,19 +3,19 @@ import React from 'react';
 class BasketItems extends React.Component {
     
     render() {
-      
+        
         return (
-            <div class="trolley-tile-content">
-                <div class="col-md-3">
-                    <img src="https://secure.ce-tescoassets.com/assets/PL/769/5051007044769/ShotType1_135x135.jpg" class="product-image" alt="">
+            <div className="trolley-tile-content">
+                <div className="col-md-3">
+                    <img src="https://secure.ce-tescoassets.com/assets/PL/769/5051007044769/ShotType1_135x135.jpg" className="product-image" alt=""/>
                 </div>
-                <div class="col-md-6">
-                    <div class="description">
+                <div className="col-md-6">
+                    <div className="description">
                         <a>Tesco Loves Baby Ultra Dry 3 Midi 4-9 kg Nappies 128 Pieces</a>
                     </div>
-                    <div class="price">0,45 zł/Piece</div>
+                    <div className="price">0,45 zł/Piece</div>
                 </div>
-                <div class="col-md-3">qwer
+                <div className="col-md-3">qwer
                 </div>
             </div>
         )
