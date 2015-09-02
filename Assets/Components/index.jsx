55b21fc73807960c986190cac7bcb7ea044ca75b
Arunkumar <arunkumar.shivanna@in.tesco.com>
2015-09-02 07:31:00 +0100

    login light box added
    some changes still need to be done

--- Assets/Components/index.jsx
+++ Assets/Components/index.jsx
@@ -3,6 +3,8 @@ import Result from './searchConponent';
 import Logo from './logo';
 import Footer from './footer';
 import Navigation from './navigation';
+import Login from './Login';
+React.render(<Login />, document.getElementById('login'));
 React.render(<Logo />, document.getElementById('logo'));
 React.render(<Result />, document.getElementById('search'));
 React.render(<Navigation />, document.getElementById('navigationMenu'));
