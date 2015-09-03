45511a73c2538bdc9f911796700f7b7c8ecf8172
iprameshkumar <iprameshkumar@gmail.com>
2015-09-03 07:32:04 +0100

    Added Basket Items

--- Assets/Components/basketItems.jsx
+++ Assets/Components/basketItems.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 class BasketItems extends React.Component {
     
     render() {
-        
+      
         return (
-            <div className="trolley-tile-content">
+            <div className="basket-tile-content">
                 <div className="col-md-3">
-                    <img src="https://secure.ce-tescoassets.com/assets/PL/769/5051007044769/ShotType1_135x135.jpg" className="product-image" alt=""/>
+                    <img src="https://secure.ce-tescoassets.com/assets/PL/769/5051007044769/ShotType1_135x135.jpg" className="product-image" alt="" />
                 </div>
                 <div className="col-md-6">
                     <div className="description">
@@ -15,7 +15,29 @@ class BasketItems extends React.Component {
                     </div>
                     <div className="price">0,45 zł/Piece</div>
                 </div>
-                <div className="col-md-3">qwer
+                <div className="col-md-3 placeholder">
+                    <button type="button" className="btn btn-danger pull-right" aria-label="Left Align">
+                        <span className="glyphicon glyphicon glyphicon-trash" aria-hidden="true"></span>
+                    </button>
+                    <div className="quantity">            
+                        <div className="input-group input-group-sm col-lg-8 pull-right margin5">
+                          <span className="input-group-addon" id="sizing-addon3">Total</span>
+                          <input type="text" className="form-control text-right" placeholder="24" aria-describedby="sizing-addon3" />
+                        </div>
+                        <div className="row">
+                            <div className="col-lg-8 pull-right">
+                                <div className="input-group">
+                                    <span className="input-group-btn">
+                                        <button className="btn btn-primary" type="button">-</button>
+                                    </span>
+                                    <input type="text" className="form-control text-center" placeholder="1" />
+                                    <span className="input-group-btn">
+                                        <button className="btn btn-primary" type="button">+</button>
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
