bcc7a9e6ae19ca8d02b11c645975f5e0d8cbc13b
Rakesh Nayak <Rakesh.Nayak@in.tesco.com>
2015-09-06 20:09:46 +0530

    Changed Cookie expiry time to 1/2 an hour

--- Assets/Components/Helper/helper.js
+++ Assets/Components/Helper/helper.js
@@ -12,7 +12,7 @@ export function getCookie(cname) {
 
 export function setCookie(cname, cvalue, exdays) {
     var d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    d.setTime(d.getTime() + (exdays*0.5*60*60*1000));
     var expires = "expires="+d.toUTCString();
     document.cookie = cname + "=" + cvalue + "; " + expires;
 }
